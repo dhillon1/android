b350fb0d3ae4ab5e64f783baaaff1ea1ef43172f
dhillon1 <simrandhillon5486@gmail.com>
2020-04-02 18:15:31 -0400

    Version 2 : Data fetched from ip info and ipify api and displayed in ui of main activity.

--- app/src/main/java/com/sd/project/MainActivity.java
+++ app/src/main/java/com/sd/project/MainActivity.java
@@ -1,12 +1,16 @@
 package com.sd.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -22,11 +26,17 @@ import com.google.firebase.auth.FirebaseAuth;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static android.view.View.GONE;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputIp;
     private Button searchIp, myIpSearch;
     private String info;
+    private TextView ip,city,region,country,loc,timezone,demoInfo;
+    private ConstraintLayout constraintLayoutDemo;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +45,23 @@ public class MainActivity extends AppCompatActivity {
         inputIp = findViewById(R.id.mainEditText);
         searchIp = findViewById(R.id.mainSearch);
         myIpSearch = findViewById(R.id.mainMyInfo);
+        constraintLayoutDemo = findViewById(R.id.constraitLayoutDemo);
+        ip = findViewById(R.id.demoIp);
+        city = findViewById(R.id.demoCity);
+        region = findViewById(R.id.demoRegion);
+        country = findViewById(R.id.demoCountry);
+        loc = findViewById(R.id.demoLocation);
+        timezone = findViewById(R.id.demoTimezone);
+        demoInfo = findViewById(R.id.demoInfo);
+        constraintLayoutDemo.setVisibility(GONE);
+        demoInfo.setVisibility(GONE);
+
 
         searchIp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                constraintLayoutDemo.setVisibility(GONE);
+                demoInfo.setVisibility(GONE);
                 info = inputIp.getText().toString().trim();
 
                 if(info.isEmpty()){
@@ -47,6 +70,53 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                final RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                JsonObjectRequest jsonObject4 = new JsonObjectRequest(Request.Method.GET,"https://ipinfo.io/"+info+"?token=9ffaf2ee5c712a", null,
+                        new Response.Listener<JSONObject>()
+                        {
+                            @Override
+                            public void onResponse(JSONObject response4) {
+
+
+                                try {
+                                    constraintLayoutDemo.setVisibility(View.VISIBLE);
+                                    demoInfo.setVisibility(GONE);
+                                    ip.setText(response4.getString("ip"));
+                                    city.setText(response4.getString("city"));
+                                    region.setText(response4.getString("region"));
+                                    country.setText(response4.getString("country"));
+                                    loc.setText(response4.getString("loc"));
+                                    timezone.setText(response4.getString("timezone"));
+
+                                } catch (JSONException e) {
+                                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                demoInfo.setVisibility(View.VISIBLE);
+                                constraintLayoutDemo.setVisibility(GONE);
+                                demoInfo.setText("Internet not connected or IP not formatted properly. The syntax of IP should be x.x.x.x");
+                            }
+                        }
+                );
+                queue.add(jsonObject4);
+
+
+
+
+            }
+        });
+
+        myIpSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                constraintLayoutDemo.setVisibility(GONE);
+                demoInfo.setVisibility(GONE);
                 final RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
 
                 String url ="https://api.ipify.org?format=json";
@@ -59,21 +129,35 @@ public class MainActivity extends AppCompatActivity {
                             public void onResponse(JSONObject response) {
 
                                 try {
-                                    String ip = response.getString("ip");
-                                    JsonObjectRequest jsonObject4 = new JsonObjectRequest(Request.Method.GET,"https://ipinfo.io/"+ip+"?token=9ffaf2ee5c712a", null,
+                                    final String name = response.getString("ip");
+                                    JsonObjectRequest jsonObject4 = new JsonObjectRequest(Request.Method.GET,"https://ipinfo.io/"+name+"?token=9ffaf2ee5c712a", null,
                                             new Response.Listener<JSONObject>()
                                             {
                                                 @Override
-                                                public void onResponse(JSONObject response) {
+                                                public void onResponse(JSONObject response4) {
 
-                                                    Log.d("Response", "done");
+
+                                                    try {
+                                                        constraintLayoutDemo.setVisibility(View.VISIBLE);
+                                                        ip.setText(response4.getString("ip"));
+                                                        city.setText(response4.getString("city"));
+                                                        region.setText(response4.getString("region"));
+                                                        country.setText(response4.getString("country"));
+                                                        loc.setText(response4.getString("loc"));
+                                                        timezone.setText(response4.getString("timezone"));
+
+
+                                                    } catch (JSONException e) {
+                                                        Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+
+                                                    }
                                                 }
                                             },
                                             new Response.ErrorListener()
                                             {
                                                 @Override
                                                 public void onErrorResponse(VolleyError error) {
-                                                    Toast.makeText(MainActivity.this, error.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                                                    Toast.makeText(MainActivity.this, "1",Toast.LENGTH_SHORT).show();
                                                 }
                                             }
                                     );
@@ -87,23 +171,25 @@ public class MainActivity extends AppCompatActivity {
                         {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(MainActivity.this, error.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                                demoInfo.setVisibility(View.VISIBLE);
+                                constraintLayoutDemo.setVisibility(GONE);
+                                demoInfo.setText("Internet not connected");
                             }
                         }
                 );
 
                 queue.add(jsonObject);
 
-
-            }
-        });
-
-        myIpSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
 }
